28699c3428678fd2f793f1c25a0d5b5754126e14
Rohith Palakirti <rohithkp99@gmail.com>
2022-10-21 13:12:47 +1000

    Update AppComponent.java

--- AppComponent.java
+++ AppComponent.java
@@ -24,10 +24,7 @@ import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-// Own import statements
 import org.onosproject.net.*;
 import org.onosproject.net.device.DeviceService;
 import org.onosproject.net.device.PortStatistics;
@@ -35,16 +32,7 @@ import org.onosproject.net.behaviour.DeviceCpuStats;
 
 import java.util.Iterator;
 import java.util.List;
-// Own import statements
-
-import java.util.Dictionary;
-import java.util.Properties;
-
-import static org.onlab.util.Tools.get;
-
-// custom imports
 import java.io.*;
-// import com.opencsv.CSVWriter;
 import java.text.SimpleDateFormat;
 import java.time.*;
 
@@ -70,93 +58,58 @@ public class AppComponent implements SomeInterface {
     @Activate
     protected void activate() throws IOException {
         cfgService.registerProperties(getClass());
-        log.info("Hello World: starting app");
+        log.info("Starting app");
 
-        if (deviceService == null) {
-            log.info("DeviceService is null");
-
-            DeviceCpuStats stats = new DeviceCpuStats();
-
-            if (stats != null) {
-                log.info("DeviceCpuStats: " + stats.getUsed());
-            } else {
-                log.info("DeviceCpuStats is null");
-            }
-
-            return;
-        }
         File file = new File("/home/sdn/data.csv");
         FileWriter outputfile = new FileWriter(file);
-
-        // CSVWriter writer = new CSVWriter(outputfile);
         String header = "timestamp, device, port, bytesReceived, bytesSent\n";
-        // writer.writeNext(header);
         log.info("Writing header");
-	outputfile.write(header);
+        outputfile.write(header);
+
         while (true) {
             Iterable<Device> devices = deviceService.getDevices();
 
             if (!devices.iterator().hasNext()) {
                 log.info("Empty list of devices");
-
                 return;
             }
             try {
-                // log.info("Writing header");
-                // outputfile.write(header);
                 for (Device d : devices) {
-                    //log.info("Device ID: " + d.id().toString());
 
+                    // log.info("Device ID: " + d.id().toString());
                     List<Port> ports = deviceService.getPorts(d.id());
+
                     for (Port p : ports) {
-                        //log.info("Getting info for port " + p.number());
-
+                        // log.info("Getting info for port " + p.number());
                         PortStatistics portStat = deviceService.getDeltaStatisticsForPort(d.id(), p.number());
-                        // PortStatistics portDeltaStat = deviceService.getDeltaStatisticsForPort(d.id(), p.number());
-
                         if (portStat != null) {
-                            //log.info("portStat bytes received: " + portStat.bytesReceived());
+                            // log.info("portStat bytes received: " + portStat.bytesReceived());
                         } else {
-                            ///log.info("Unable to read portStat");
+                            // log.info("Unable to read portStat");
                             continue;
                         }
-
-                        //if (portDeltaStat != null) {
-                            //log.info("portDeltaStat bytes received: " + portDeltaStat.bytesReceived());
-                        //} else {
-                            //log.info("Unable to read portDeltaStat");
-                          //  continue;
-                        //}
-                        // String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
-                        String timeStamp =  LocalDateTime.now().toString();
+                        String timeStamp = LocalDateTime.now().toString();
                         String[] stats = { timeStamp, d.id().toString(), p.number().toString(),
                                 Integer.toString((int) portStat.bytesReceived()),
                                 Integer.toString((int) portStat.bytesSent()) };
-                        // writer.writeNext(stats);
                         String str = String.join(",", stats);
                         str = str + "\n";
-                        //log.info("Writing: " + str);
+
+                        // log.info("Writing: " + str);
                         outputfile.write(str);
-                        //Thread.sleep(5000); 
-
                     }
-
                 }
                 try {
-			Thread.sleep(1000);
-		    } catch (InterruptedException e) {
-			break;
-		    }
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    break;
+                }
                 continue;
             } catch (Exception e) {
-                
-                log.info("Exception!!!" + e);
-
+                log.info("Exception:" + e);
             }
         }
-        // outputfile.close();
-        // writer.close();
-
+        outputfile.close();
     }
 
     @Deactivate
@@ -178,5 +131,4 @@ public class AppComponent implements SomeInterface {
     public void someMethod() {
         log.info("Invoked");
     }
-
 }
